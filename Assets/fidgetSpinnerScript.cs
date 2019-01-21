21d650e92a01fb78bc8a742f53b2eab253322521
noahcoolboy <noah.nuelandt@gmail.com>
2019-01-21 16:24:29 +0100

    uploaded

---
+++ Assets/fidgetSpinnerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+public class fidgetSpinnerScript : MonoBehaviour {
+    public KMNeedyModule needy;
+    
+    private bool active;
+    public KMSelectable spinner;
+	// Use this for initialization
+	void Start () {
+        needy.OnNeedyActivation += activation;
+        needy.OnNeedyDeactivation += OnNeedyDeactivation;
+        needy.OnTimerExpired += OnTimerExpired;
+        spinner.OnInteract += spin;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (active)
+        {
+            transform.Rotate(Vector3.up * needy.GetNeedyTimeRemaining());
+        }
+        
+	}
+    void activation()
+    {
+        active = true;
+    }
+    protected void OnNeedyDeactivation()
+    {
+        active = false;
+    }
+    protected void OnTimerExpired()
+    {
+        needy.OnStrike();
+        active = false;
+    }
+    
+    protected bool spin()
+    {
+        needy.SetNeedyTimeRemaining(needy.GetNeedyTimeRemaining() + 2);
+        Debug.Log(needy.CountdownTime);
+        return false;
+    }
+}

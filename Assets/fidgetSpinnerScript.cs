fea629b195f168d1df1e4aa55157ca0d00890216
ryaninator81 <45949859+ryaninator81@users.noreply.github.com>
2019-03-27 20:08:49 -0600

    Fix exploit, and clean up code

--- Assets/fidgetSpinnerScript.cs
+++ Assets/fidgetSpinnerScript.cs
@@ -1,28 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using KModkit;
-public class fidgetSpinnerScript : MonoBehaviour {
+public class fidgetSpinnerScript : MonoBehaviour
+{
     public KMNeedyModule needy;
-    
+
     private bool active;
     public KMSelectable spinner;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         needy.OnNeedyActivation += activation;
         needy.OnNeedyDeactivation += OnNeedyDeactivation;
         needy.OnTimerExpired += OnTimerExpired;
         spinner.OnInteract += spin;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (active)
         {
             transform.Rotate(Vector3.up * needy.GetNeedyTimeRemaining());
         }
-        
-	}
+
+    }
     void activation()
     {
         active = true;
@@ -36,10 +39,14 @@ public class fidgetSpinnerScript : MonoBehaviour {
         needy.OnStrike();
         active = false;
     }
-    
+
     protected bool spin()
     {
-        needy.SetNeedyTimeRemaining(needy.GetNeedyTimeRemaining() + 2);
+        if (needy.GetNeedyTimeRemaining() > 40)
+        needy.SetNeedyTimeRemaining(needy.GetNeedyTimeRemaining() + 0);
+
+        if ((needy.GetNeedyTimeRemaining() < 40))
+            needy.SetNeedyTimeRemaining(needy.GetNeedyTimeRemaining() + 2);
         Debug.Log(needy.CountdownTime);
         return false;
     }
